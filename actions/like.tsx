e932ea75cb8d45d3d6213f8d210279fdcf998994
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-07-05 16:01:55 -0700

    Added unlike functionality

--- actions/like.tsx
+++ actions/like.tsx
@@ -1,7 +1,19 @@
+const unlikeItem = (store, item) => {
+  item.liked = false;
+
+  // Will improve this once results have IDs attached to them
+  const newLiked = store.state.liked.filter(likedItem => (
+    likedItem.neighborhood != item.neighborhood &&
+    likedItem.street != item.street &&
+    likedItem.avgValue != item.avgValue
+  ));
+
+  store.setState({ liked: newLiked });
+}
+
 export const likeItem = (store, item) => {
-  // TODO: Unlike
   if (item.liked) {
-    return;
+    return unlikeItem(store, item);
   }
 
   item.liked = true;
