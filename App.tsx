ac65faaa3325aaafd415f633483ce73055bd08a2
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-07-07 16:56:47 -0700

    Starting to style the bottom tab navigation bar

--- App.tsx
+++ App.tsx
@@ -1,23 +1,60 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { StatusBar, Image, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer, BottomTabBar } from 'react-navigation';
 import { ThemeProvider } from 'styled-components';
 import { Scan, History, Liked } from './screens';
 import { Theme as theme } from './shared';
 
+import { history, like, scan } from './assets';
+
+const TabIcon = ({ tab }) => {
+  let iconSource = null;
+
+  switch(tab) {
+    case 'history':
+      iconSource = history;
+      break;
+    case 'scan':
+      iconSource = scan;
+      break;
+    case 'liked':
+      iconSource = like;
+      break;
+  }
+
+  return (
+    <Image source={iconSource} />
+  )
+}
+
 const TabNavigator = createBottomTabNavigator({
-  History,
-  Scan,
-  Liked,
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarIcon: () => <TabIcon tab='history' />
+    }
+  },
+  Scan: {
+    screen: Scan,
+    navigationOptions: {
+      tabBarIcon: () => <TabIcon tab='scan' />
+    }
+  },
+  Liked: {
+    screen: Liked,
+    navigationOptions: {
+      tabBarIcon: () => <TabIcon tab='liked' />
+    }
+  },
 }, {
-  initialRouteName: 'Scan'
+  initialRouteName: 'Scan',
 });
 
 const AppContainer = createAppContainer(TabNavigator);
 
 export default () => {
   StatusBar.setBarStyle('light-content', true);
-  
+
   return (
     <ThemeProvider theme={theme}>
       <AppContainer />
---
+++ assets/index.tsx
@@ -0,0 +1,9 @@
+const history = require('./history.png');
+const like = require('./like.png');
+const scan = require('./scan.png');
+
+export {
+  history,
+  like,
+  scan
+}
