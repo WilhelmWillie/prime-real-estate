b28e89bdd3a25fc80eb49edd8b319e2f923c6758
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-07-07 23:29:37 -0700

    Styled up the navigation bar

--- App.tsx
+++ App.tsx
@@ -2,29 +2,37 @@ import React from 'react';
 import { StatusBar, Image, View } from 'react-native';
 import { createBottomTabNavigator, createAppContainer, BottomTabBar } from 'react-navigation';
 import { ThemeProvider } from 'styled-components';
+import { FontAwesome } from '@expo/vector-icons';
+
 import { Scan, History, Liked } from './screens';
 import { Theme as theme } from './shared';
 
-import { history, like, scan } from './assets';
+import { scan } from './assets';
 
 const TabIcon = ({ tab }) => {
-  let iconSource = null;
-
-  switch(tab) {
-    case 'history':
-      iconSource = history;
-      break;
-    case 'scan':
-      iconSource = scan;
-      break;
-    case 'liked':
-      iconSource = like;
-      break;
+  if (tab === 'history') {
+    return <FontAwesome name="history" size={32} color='#AEAEAE' />
+  } else if (tab === 'liked') {
+    return <FontAwesome name="heart" size={32} color='#AEAEAE' />
+  } else if (tab === 'scan') {
+    return (
+      <View
+        style={{
+          backgroundColor: '#FF9D55',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: 100,
+          paddingHorizontal: 30,
+          borderTopStartRadius: 8,
+          borderTopEndRadius: 8,
+        }}
+      >
+        <Image source={scan} width={32} height={32} />
+      </View>
+    );
   }
 
-  return (
-    <Image source={iconSource} />
-  )
+  return null;
 }
 
 const TabNavigator = createBottomTabNavigator({
@@ -48,6 +56,21 @@ const TabNavigator = createBottomTabNavigator({
   },
 }, {
   initialRouteName: 'Scan',
+  tabBarOptions: {
+    showLabel: false,
+    style: {
+      height: 80,
+      paddingVertical: 5,
+      borderTopColor: 'transparent',
+      shadowOffset: {
+        width: 0,
+        height: -3
+      },
+      shadowColor: '#000',
+      shadowOpacity: 0.15,
+      shadowRadius: 5
+    }
+  },
 });
 
 const AppContainer = createAppContainer(TabNavigator);
